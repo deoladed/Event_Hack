412172718e2f432f6a1bcd9ce363787657aeeecd
deoladed <leo.robert.mk@gmail.com>
2019-02-12 01:14:26 +0100

    Les tests passent, TOUS, genre incroyable

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.perform_deliveries = false
+  config.action_mailer.perform_deliveries = true
 
 # Defaults to:
 # config.action_mailer.sendmail_settings = {
